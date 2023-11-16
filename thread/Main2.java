df9db7e08c74017cc658f06f57177ff0298ff424
Nurlis <kimbiletovnurlis03@gmail.com>
2023-11-16 22:44:13 +0600

    continue to learn thread

--- thread/Main2.java
+++ thread/Main2.java
@@ -1,7 +1,34 @@
 package thread;
 
+class MyThread2 extends Thread {     //MyThread теперь поток
+    public void run() {
+        for (int i = 0; i < 400; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello from MyThread " + i);
+        }
+    }
+}
+
+// Второй способ, где класс реализует интерфейс Runnable
+class Runner implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 400; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello from MyThread " + i);
+        }
+    }
+}
 public class Main2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Многопоточность позволяет нам выполнять какой-то код паралельно с другим кодом
         // Многопоточность может работать если у вас одноядерный или многоядерный компьютер
         // Многопоточность в java представлена в виде виртуальной паралельности
@@ -12,10 +39,19 @@ public class Main2 {
 
         // 2 способа создания потоков:
         // 1. создаем класс и в этом классе наследуемся от класса Thread
+        // MyThread2 myThread = new MyThread2();
+        // myThread.start();
+
+        // MyThread2 myThread2 = new MyThread2();
+        // myThread2.start();
+
+        // System.out.println("I am going to sleep");
+        // Thread.sleep(3000);
+        // System.out.println("I am wake!");
+
+        Thread thread = new Thread(new Runner());
+        thread.start();
+
+        System.out.println("Hello from main thread");
     }
 }
-
-class MyThread extends Thread {     //MyThread теперь поток
-    
-}
-
