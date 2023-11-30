7dd8d02a7d34f8f290cdec887cf5501bbe041a65
Nurlis <kimbiletovnurlis03@gmail.com>
2023-11-30 23:40:02 +0600

    continue to learn maven

---
+++ gradleTutorial/src/test/java/com/tomgregory/GradleTutorialTest.java
@@ -0,0 +1,12 @@
+package com.tomgregory;
+
+import com.tomgregory.HelloWorld;
+import org.junit.Test;
+
+public class GradleTutorialTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        HelloWorld.main(new String[]{});
+    }
+}
